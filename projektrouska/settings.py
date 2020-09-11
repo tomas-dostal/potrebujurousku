ac53d799b7db41dfbe6026181d2edc038c80c2a1
Tomas Dostal <admin@potrebujurousku.cz>
2020-09-11 02:15:16 +0200

    err fix

--- projektrouska/settings.py
+++ projektrouska/settings.py
@@ -5,5 +5,3 @@ if(socket.gethostname() == "inspiron-13-5368"):
     from projektrouska.local_settings import *
 else:
     from projektrouska.production_settings import *
-
-print(SECURE_SSL_REDIRECT)
