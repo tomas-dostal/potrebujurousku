0cd980d7742dbc895ac42118a9d26a17b4e4b1c0
tomas-dostal <tomas.dostal.opava@gmail.com>
2021-01-04 17:39:24 +0100

    Update_stats rewrite, add unit tests

---
+++ projektrouska/api/test_update_stats.py
@@ -0,0 +1,109 @@
+import json
+import unittest
+
+from django.core.serializers.json import DjangoJSONEncoder
+
+from projektrouska.functions import return_as_array
+from .update_stats import get_stats
+from django.db import connection
+
+from django.test import TestCase
+from projektrouska.aktualnost.updatecheck import UpdateCheck
+from projektrouska.models import *
+
+update_controller = UpdateCheck()
+
+
+class UpdateStatsTest(TestCase):
+
+    def test_query_equals_edge_values(self):
+        date3 = (datetime.datetime.now() - datetime.timedelta(days=50)).replace(tzinfo=utc)
+        date2 = (datetime.datetime.now() - datetime.timedelta(days=2)).replace(tzinfo=utc)
+
+        UpdateLogs(
+            date_updated=date2,
+            checksum="test",
+            comment="older one, displayed in groupby",
+            up_to_date_percents=12).save()
+
+        UpdateLogs(
+            date_updated=date3,
+            checksum="test3",
+            comment="Too old to be displayed",
+            up_to_date_percents=42).save()
+
+        res1 = json.loads(get_stats(show_last_days=50))["data"]
+        self.assertEqual(len(res1), 2)
+
+    def test_query_equals_sql(self):
+        date1 = (datetime.datetime.now()
+                 - datetime.timedelta(days=1)).replace(tzinfo=utc)
+        date2 = (datetime.datetime.now()
+                 - datetime.timedelta(days=2)).replace(tzinfo=utc)
+        date3 = (datetime.datetime.now()
+                 - datetime.timedelta(days=50)).replace(tzinfo=utc)
+
+        g1 = UpdateLogs(
+            date_updated=date1,
+            checksum="test",
+            comment="older one, displayed in groupby",
+            up_to_date_percents=12).save()
+
+        g2 = UpdateLogs(
+            date_updated=date2,
+            checksum="test",
+            comment="older one, displayed in groupby",
+            up_to_date_percents=12).save()
+
+        g3 = UpdateLogs(
+            date_updated=date3,
+            checksum="test3",
+            comment="Too old to be displayed",
+            up_to_date_percents=42).save()
+
+        res1 = get_stats(show_last_days=14)
+        show_from = (datetime.datetime.now()
+                     - datetime.timedelta(days=14)).replace(tzinfo=utc)
+
+        qu = '''
+                select * from
+                (
+                select min(date_updated) as date_updated,
+                    "checksum"    ,                
+                    "comment", 
+                    up_to_date_percents, 
+                    missing_count, 
+                    change_link_count,
+                    outdated_count, 
+                    total_changes
+                from update_logs
+                group by 
+                    "checksum",
+                    "comment", 
+                    up_to_date_percents, 
+                    missing_count, 
+                    change_link_count, 
+                    outdated_count, 
+                    total_changes 
+                order by  date_updated
+                
+                    ) as ali where  date_updated >= %s
+                '''
+
+        with connection.cursor() as cursor:
+            cursor.execute(qu, [show_from])
+
+            # res1 is returned exactly this way, so if data is the same, it should be equal
+            res2 = json.dumps(
+                {
+                    "data": list(return_as_array(cursor.fetchall(), cursor.description))
+                },
+                sort_keys=True,
+                indent=1,
+                cls=DjangoJSONEncoder)
+
+            self.assertEqual(res2, res1)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- projektrouska/api/update_stats.py
+++ projektrouska/api/update_stats.py
@@ -1,73 +1,71 @@
+import datetime
 import json
 from django.http import JsonResponse
 from django.db import connection
+from pytz import utc
+from django.core.serializers.json import DjangoJSONEncoder
 
-def get_update_stats(request):
-    args = request.GET.copy()
-    show_last_days = 7
-    try:
-        show_last_days = int(args.get("days_old", "31"))
-    except: # bad request
-        return JsonResponse({'error': 'bad request. Optional parameters: "days_old"'}, safe=False)
+from projektrouska.models import UpdateLogs
+from django.db.models import Min
 
 
-    #qu = '''
+def get_stats(show_from=None, show_last_days=31):
+    # qu = '''
     #    select * from(
-    #    select min(DATE_UPDATED) as DATE_UPDATED, CHECKSUM, POZNAMKA, AKTUALNOST, CHYBI_POCET, CHYBI_POLE, ZMENA_LINK_POCET, ZMENA_LINK_POLE, ODSTRANIT_POCET, ODSTRANIT_POLE, CELK_ZMEN from info
-    #    group by CHECKSUM, POZNAMKA, AKTUALNOST, CHYBI_POCET, CHYBI_POLE, ZMENA_LINK_POCET, ZMENA_LINK_POLE, ODSTRANIT_POCET, ODSTRANIT_POLE, CELK_ZMEN
+    #    select min(DATE_UPDATED) as DATE_UPDATED, CHECKSUM, POZNAMKA, AKTUALNOST,
+    #    CHYBI_POCET, CHYBI_POLE, ZMENA_LINK_POCET, ZMENA_LINK_POLE, ODSTRANIT_POCET,
+    #    ODSTRANIT_POLE, CELK_ZMEN from info
+    #    group by CHECKSUM, POZNAMKA, AKTUALNOST, CHYBI_POCET, CHYBI_POLE, ZMENA_LINK_POCET,
+    #    ZMENA_LINK_POLE, ODSTRANIT_POCET, ODSTRANIT_POLE, CELK_ZMEN
     #    order by  DATE_UPDATED) where DATE_UPDATED >= trunc(sysdate)  - :old
     #    '''
-    qu = '''
-            select * from(
-            select min(DATE_UPDATED) as DATE_UPDATED, CHECKSUM, POZNAMKA, AKTUALNOST, CHYBI_POCET, ZMENA_LINK_POCET, ODSTRANIT_POCET, CELK_ZMEN from info
-            group by CHECKSUM, POZNAMKA, AKTUALNOST, CHYBI_POCET, ZMENA_LINK_POCET, ODSTRANIT_POCET, CELK_ZMEN
-            order by  DATE_UPDATED) where DATE_UPDATED >= trunc(sysdate)  - :old
-            '''
-    with connection.cursor() as cursor:
-        cursor.execute(qu, {"old": show_last_days})
+    if not show_from:
+        show_from = (datetime.datetime.now()
+                     - datetime.timedelta(days=show_last_days)
+                     - datetime.timedelta(seconds=10)).replace(tzinfo=utc)
 
-        from projektrouska.functions import return_as_array
-        array = return_as_array(cursor.fetchall(), cursor.description)
-        #jsonStr = json.dumps(array)
-        #if (jsonStr == []):
-        #    return JsonResponse({'data': 'empty'}, safe=False)
+    stats = UpdateLogs.objects.values(
+        "checksum",
+        "comment",
+        "up_to_date_percents",
+        "missing_count",
+        # "missing_json",
+        "change_link_count",
+        # "change_link_json",
+        "outdated_count",
+        # "outdated_json",
+        "total_changes").annotate(
+        date_updated=Min('date_updated')).filter(date_updated__gte=show_from)
 
-        from django.core.serializers.json import DjangoJSONEncoder
-        dum = json.dumps(
-            {"data": array},
-            sort_keys=True,
-            indent=1,
-            cls=DjangoJSONEncoder)
+    return json.dumps(
+        {
+            "data": list(stats)
+        },
+        sort_keys=True,
+        indent=1,
+        cls=DjangoJSONEncoder
+    )
 
-        return JsonResponse(dum, safe=False)
+
+def get_update_stats(request, show_from=None, show_last_days=31):
+    args = request.GET.copy()
+
+    try:
+        days = int(args.get("days_old", "31"))
+        show_from = datetime.datetime.now().replace(tzinfo=utc) - datetime.timedelta(days=days)
+    except ValueError:  # bad request
+        return JsonResponse({'error': 'bad request. Optional parameters: "days_old"'}, safe=False)
+
+    if not show_from:
+        show_from = datetime.datetime.now().replace(tzinfo=utc) \
+                    - datetime.timedelta(days=show_last_days)
+
+    return JsonResponse(get_stats(show_from), safe=False)
+
 
 def get_all_update_stats(request):
     args = request.GET.copy()
-    show_last_days = 7
-    try:
-        show_last_days = int(args.get("days_old", "31"))
-    except: # bad request
-        return JsonResponse({'error': 'bad request. Optional parameters: "days_old"'}, safe=False)
 
+    min_date = UpdateLogs.objects.aggregate(Min('date_updated'))
 
-    qu = '''select * from(
-            select DATE_UPDATED, CHECKSUM, POZNAMKA, AKTUALNOST, CHYBI_POCET, ZMENA_LINK_POCET, ODSTRANIT_POCET, CELK_ZMEN from info
-            order by  DATE_UPDATED) where DATE_UPDATED >= (trunc(sysdate)  - :old)
-            '''
-    with connection.cursor() as cursor:
-        cursor.execute(qu, {"old": show_last_days})
-
-        from projektrouska.functions import return_as_array
-        array = return_as_array(cursor.fetchall(), cursor.description)
-        #jsonStr = json.dumps(array)
-        #if (jsonStr == []):
-        #    return JsonResponse({'data': 'empty'}, safe=False)
-
-        from django.core.serializers.json import DjangoJSONEncoder
-        dum = json.dumps(
-            {"data": array},
-            sort_keys=True,
-            indent=1,
-            cls=DjangoJSONEncoder)
-
-        return JsonResponse(dum, safe=False)
+    return get_update_stats(request, min_date)
