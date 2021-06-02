2e5510a69e0f5a277c63d57752d0631967d367a7
tomas-dostal <tomas.dostal.opava@gmail.com>
2021-06-02 02:09:11 +0200

    Update db export to fit new structure

--- projektrouska/api/search.py
+++ projektrouska/api/search.py
@@ -11,14 +11,14 @@ def find_place_by_name(request):
     if (len(query_string) < 2):
         return
 
-    states = State.objects.filter(name__istartswith=query_string)
+    states = State.objects.filter(name__istartswith=query_string)[:10]
 
-    regions = Region.objects.filter(name__istartswith=query_string)
-    districts = District.objects.filter(name__istartswith=query_string)
-    nuts4 = Nuts4.objects.filter(name__istartswith=query_string)
-    cities = City.objects.filter(name__istartswith=query_string)
-
+    regions = Region.objects.filter(name__istartswith=query_string)[:10]
+    districts = District.objects.filter(name__istartswith=query_string)[:10]
+    nuts4 = Nuts4.objects.filter(name__istartswith=query_string)[:10]
+    cities = City.objects.filter(name__istartswith=query_string)[:10]
     merged = list(states) + list(regions) + list(districts) + list(nuts4) + list(cities)
+    merged = merged[:10]
 
     json_str = json.dumps([place.place_as_dict() for place in merged])
 
